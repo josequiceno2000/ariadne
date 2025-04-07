74bcb358b18254045b316fa7f4ef906fcfdb1733
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-04-07 16:04:26 -0500

    Add tkinter to reqs and setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Ariadne",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "tkinter>=8.6",
+    ],
+    entry_points={
+        "console_scripts": [
+            "ariadne=ariadne.ui.app:main",
+        ],
+    },
+    author="Jose Quiceno",
+    author_email="josequiceno000@gmail.com",
+    description="Advanced maze generation and solving visualization toolkit",
+    keywords="maze, algorithms, visualization, pathfinding",
+    python_requires=">=3.10.6",
+)
