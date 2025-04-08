2777513df9c1b2d18a7d79546d655bae3bf51783
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-04-07 23:05:03 -0500

    Refactor Window class

--- ariadne/ui/maze_canvas.py
+++ ariadne/ui/maze_canvas.py
@@ -1,7 +1,7 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int):
         self.__root = Tk()
         self.__root.title("Ariadne")
         self.__canvas = Canvas(self.__root, width=width, height=height)
@@ -10,15 +10,18 @@ class Window:
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
     def redraw(self):
+        """Redraws window while running"""
         self.__root.update_idletasks()
         self.__root.update()
 
     def wait_for_close(self):
+        """Sets root to run and redraws while running"""
         self.__running = True
         while self.__running:
             self.redraw()
     
     def close(self):
+        """Closes the window"""
         self.__running = False
 
 
