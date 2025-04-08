963292ad9e397a7236cda02bd38aeb0cc9d15513
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-04-07 21:42:45 -0500

    Add __init__.py

---
+++ ariadne/__init__.py
@@ -0,0 +1,33 @@
+"""
+Ariadne 
+-------
+
+Ariadne is a maze generation and solution toolkit with a GUI built in Tkinter. It includes implementations of DFS, BFS, A*, and an interactive mode.
+
+Import this package to get access to the core maze structures and algorithms
+"""
+
+__version__ = "1.0.0"
+
+from .core.maze import Maze
+from .core.cell import Cell
+
+from .core.algorithms.dfs import solve_with_dfs
+from .core.algorithms.bfs import solve_with_bfs
+from .core.algorithms.a_star import solve_with_astar
+
+from .ui.app import launch_app
+from .game.player import Player
+
+from .utils.benchmarking import benchmark_solver
+
+__all__ = [
+    "Maze",
+    "Cell",
+    "solve_with_dfs",
+    "solve_with_bfs",
+    "solve_with_astar",
+    "launch_app",
+    "Player",
+    "benchmark_solver"
+]
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Ariadne",
+    name="ariadne",
     version="1.0.0",
     packages=find_packages(),
     install_requires=[
-        "tkinter>=8.6",
+        
     ],
     entry_points={
         "console_scripts": [
@@ -15,6 +15,17 @@ setup(
     author="Jose Quiceno",
     author_email="josequiceno000@gmail.com",
     description="Advanced maze generation and solving visualization toolkit",
-    keywords="maze, algorithms, visualization, pathfinding",
-    python_requires=">=3.10.12",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/josequiceno2000/ariadne",
+    keywords="maze algorithms visualization pathfinding tkinter",
+    classifiers= [
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    python_requires=">=3.10",
+    include_package_data=True
 )
